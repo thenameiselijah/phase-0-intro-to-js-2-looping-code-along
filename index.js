7e2a0430046422b73dba6c54de44a7a0a8a79201
Elijah Oquendo <elijahoquendo420@gmail.com>
2023-09-23 17:02:31 -0400

    Looping lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+function writeCards(names) {
+    let thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+  
+  let names = ['Mateo', 'Chloe', 'Jonah'];
+  let messages = writeCards(names);
+  console.log(messages);
+
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  countDown(5);
